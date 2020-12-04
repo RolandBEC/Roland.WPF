79c2637d248d1c9748cc2e5be65434ba4fbeaf48
roland89340 <bec.roland89@gmail.com>
2020-12-04 21:43:58 +0100

    add many utilities

---
+++ Roland.WPF.C_Sharp_Utils/ObservableCollectionUtils.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roland.WPF.C_Sharp_Utils
+{
+    public static class ObservableCollectionUtils
+    {
+
+        public static void AddRange<TSource>(this ObservableCollection<TSource> list, IEnumerable<TSource> values, IEqualityComparer<TSource> comparer)
+        {
+            Debug.Assert(list != null);
+            if (values == null || (values.Count() == 0))
+            {
+                return;
+            }
+
+            foreach (TSource value in values)
+            {
+                if (!list.Contains(value, comparer))
+                {
+                    list.Add(value);
+                }
+            }
+        }
+
+        public static void RemoveRange<TSource>(this ObservableCollection<TSource> list, IEnumerable<TSource> values, IEqualityComparer<TSource> comparer)
+        {
+            Debug.Assert(list != null);
+            if (values == null || (values.Count() == 0))
+            {
+                return;
+            }
+
+            foreach (TSource value in values)
+            {
+                if (list.Contains(value, comparer))
+                {
+                    list.Remove(value, comparer);
+                }
+            }
+        }
+
+        public static void Remove<TSource>(this ObservableCollection<TSource> list, TSource value, IEqualityComparer<TSource> comparer)
+        {
+            Debug.Assert(list != null);
+            int index = list.IndexOf(value, comparer);
+            if (index >= 0 && index < list.Count)
+            {
+                list.RemoveAt(index);
+            }
+        }
+
+        public static int IndexOf<TSource>(this ObservableCollection<TSource> list, TSource value, IEqualityComparer<TSource> comparer)
+        {
+            Debug.Assert(list != null);
+            int index = 0;
+            foreach (TSource item in list)
+            {
+                if (comparer.Equals(item, value))
+                {
+                    return index;
+                }
+                index++;
+            }
+            return -1;
+        }
+
+        public static void Sort<TSource, TKey>(this ObservableCollection<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            IList<TSource> sortedList = source.OrderBy(keySelector).ToList();
+            Update(source, sortedList);
+        }
+
+        public static void Update<TSource>(this ObservableCollection<TSource> source, IList<TSource> newItems)
+        {
+            source.Clear();
+            foreach (TSource newItem in newItems)
+            {
+                source.Add(newItem);
+            }
+        }
+
+    }
+}
---
+++ Roland.WPF.Controls/Utils/MouseUtilities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Roland.WPF.Controls.Utils
+{
+    public class MouseUtilities
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(ref Win32Point pt);
+
+        [DllImport("user32.dll")]
+        private static extern bool ScreenToClient(IntPtr hwnd, ref Win32Point pt);
+
+        /// <summary>
+        /// Returns the mouse cursor location.  This method is necessary during
+        /// a drag-drop operation because the WPF mechanisms for retrieving the
+        /// cursor coordinates are unreliable.
+        /// </summary>
+        /// <param name="relativeTo">The Visual to which the mouse coordinates will be relative.</param>
+        public static Point GetMousePosition(Visual relativeTo)
+        {
+            Win32Point mouse = new Win32Point();
+            GetCursorPos(ref mouse);
+            return relativeTo.PointFromScreen(new Point((double)mouse.X, (double)mouse.Y));
+        }
+    }
+}
